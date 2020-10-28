bcd216acc7e68957fd3f7087f30f89f5ca213488
verrmqa <andreyqq@gmail.com>
2020-10-28 13:00:07 +0400

    feat: react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/Homepage";
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
