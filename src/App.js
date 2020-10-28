3a265148497798f8c9ce2193908f823d7c8e4ec9
verrmqa <andreyqq@gmail.com>
2020-10-28 15:17:33 +0400

    feat: header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
+import Header from "./components/Header";
 import HomePage from "./pages/Homepage";
 import ShopPage from "./pages/Shop";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
